ab592cbc4a8622b2a2b125dd9b4d9753ece50859
VladimirKogan <yang236nrn@gmal.com>
2018-01-16 23:53:00 +0200

    1111

---
+++ AndroidStudioProjects/14_01_18_Class_Work-master/app/src/main/java/com/example/vladimir/a14_01_18_class_work/ViewContactActivity.java
@@ -0,0 +1,13 @@
+package com.example.vladimir.a14_01_18_class_work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contact);
+    }
+}
